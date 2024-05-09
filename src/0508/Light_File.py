08047ba4437c5f14d2a6f636c64595fd97bd0d5f
AntonSangho <sanghoemail@gmail.com>
2024-05-09 09:58:08 +0900

    조도센서 값을 파일에 쓰기 예제 추가

--- src/0508/Light_File.py
+++ src/0508/Light_File.py
@@ -1,4 +1,4 @@
-from machine import Pin, ADC, RTC 
+from machine import Pin 
 from time import sleep
 #from ds3231_port import DS3231
 from machine import I2C
@@ -7,35 +7,22 @@ from bh1750 import BH1750
 
 led = Pin("LED", Pin.OUT)
 
-#rtc = RTC()
-
-#print(rtc.datetime())
 sdaPIN = Pin(4)
 sclPIN = Pin(5)
 
 i2c = I2C(0, sda=sdaPIN, scl=sclPIN) 
 
-#ds3231 = DS3231(i2c)
 bh1750 = BH1750(0x23, i2c)
 
-#analogue_value = machine.ADC(27)
-#VOLTAGE_DROP_FACTOR = 1 
 
 def writeLine(text):
     file = open("log.txt", "a")
     file.write(text + "\n")
     file.close()
-    #print(text)
  
 while True:
-    #reading = analogue_value.read_u16()     
     light = bh1750.measurement     
-    #voltage = reading * (4.5 / 65535) * VOLTAGE_DROP_FACTOR
-    #dateTime = ds3231.get_time()
-    #print("hour:min:sec")
-    #print(dateTime[3],dateTime[4],dateTime[5])
     print(light)
-    #writeLine(str(dateTime[3]) + ":" + str(dateTime[4]) + ":" + str(dateTime[5]) + " " + str(voltage))
     writeLine(str(light))
     led.value(1)
     sleep(0.5)
---
+++ src/0508/mywifi.py
@@ -0,0 +1,8 @@
+# wifi information for networksetting()
+
+def networksetting():
+    # Office
+    ssid = 'tresc3-2.4G'
+    password = 'tresc333'
+    return ssid, password
+
